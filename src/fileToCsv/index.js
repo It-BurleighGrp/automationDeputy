c5184289ecec9b5a87646c6a573538af9dd35770
IT BurleighGrp <it@burleighgrp.com>
2023-09-28 15:28:39 +1000

    fix: csv generator

--- src/fileToCsv/index.js
+++ src/fileToCsv/index.js
@@ -1,137 +1,80 @@
+import dayjs from 'dayjs'
 import fs from 'fs'
 
-export function fileToCsv(arrayTimesheet){
-    arrayTimesheet.filter((timesheet, index)=>{
-        if(timesheet.Employee === 'Isadora'){
-            if(index === 1){
-                try{
-                    const headerCsv = 'Name, Date, Start Time, End Time, Meal break, Total Time\n'
-                    fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, headerCsv)
-                } catch(error){
-                    console.log(error)
-                }    
-            }
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Giovanna'){
-            if(index === 1){
-                try{
-                    const headerCsv = 'Name, Date, Start Time, End Time, Meal break, Total Time\n'
-                    fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, headerCsv)
-                } catch(error){
-                    console.log(error)
-                }    
-            }
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Asami'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Jackie'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Jade'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Karmen'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Rachel'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Shiho'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Ana Carolina'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Melissa'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee === 'Nathalia'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-
-        if(timesheet.Employee === 'Danielle'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-
-        if(timesheet.Employee === 'Dalila'){
-            const testeCsv = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            try{
-                fs.appendFileSync(`./csv/${timesheet.Employee}.csv`, testeCsv)
-            } catch(error){
-                console.log(error)
-            }
-        }
-        if(timesheet.Employee!=='IT'&&timesheet.Employee!=='Isadora'&&timesheet.Employee!=='Giovanna')
+function appendCsv(employeeName, date, referenceDate, csvString, discarded){
+    if(dayjs(date) >= referenceDate && discarded === null){
         try{
-            fs.appendFileSync(`./csv/allEmployees.csv`, 
-                `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
-            )
+            fs.appendFileSync(`./csv/${employeeName}.csv`, csvString)
         } catch(error){
             console.log(error)
         }
+    }
+}
+
+export function fileToCsv(arrayTimesheet){
+    arrayTimesheet.filter((timesheet) => {
+        const auxReferenceDate = dayjs().subtract(1, 'w')
+        if(timesheet.Employee === 'Isadora'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Giovanna'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+           appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Asami'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Jackie'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Jade'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+           appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Karmen'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Rachel'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Shiho'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+           appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Ana Carolina'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+           appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Melissa'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee === 'Nathalia'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+
+        if(timesheet.Employee === 'Danielle'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+
+        if(timesheet.Employee === 'Dalila'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv(timesheet.Employee, timesheet.ReferenceDate, auxReferenceDate, csvInfo, timesheet.Discarded)
+        }
+        if(timesheet.Employee!=='IT'&&timesheet.Employee!=='Isadora'&&timesheet.Employee!=='Giovanna'){
+            const csvInfo = `${timesheet.Employee}, ${timesheet.Date}, ${timesheet.StartTime}, ${timesheet.EndTime}, ${timesheet.MealBreak}, ${timesheet.TotalTime}\n`
+            appendCsv('allEmployees', timesheet.ReferenceDate, auxReferenceDate, csvInfo)
+        }
+        
     })
+
+    console.log('teste aqui>>>>')
     
 }
--- src/reader/index.js
+++ src/reader/index.js
@@ -6,11 +6,14 @@ export function handleDataProcessing(timesheetArray){
     timesheetArray?.forEach((timesheet)=>{
         auxArray.push({
             Employee: employees(timesheet.Employee),
+            ReferenceDate: timesheet.Date,
             Date: dayjs(timesheet.Date).format('DD/MM/YYYY'),
-            StartTime: `${new Date(timesheet.StartTime*1000).getHours() < 10 ? `0${new Date(timesheet.StartTime*1000).getHours()}`: `${new Date(timesheet.StartTime*1000).getHours()}`}:${new Date(timesheet.StartTime*1000).getMinutes()}`, 
-            EndTime: `${new Date(timesheet.EndTime*1000).getHours() < 10 ? `0${new Date(timesheet.EndTime*1000).getHours()}`: `${new Date(timesheet.EndTime*1000).getHours()}`}:${new Date(timesheet.EndTime*1000).getMinutes()}`,            
+            StartTime: `${new Date(timesheet.StartTime*1000).getHours() < 10 ? `0${new Date(timesheet.StartTime*1000).getHours()}` : new Date(timesheet.StartTime*1000).getHours()}:${new Date(timesheet.StartTime*1000).getMinutes() < 10 ? `0${new Date(timesheet.StartTime*1000).getMinutes()}` : new Date(timesheet.StartTime*1000).getMinutes()}`,
+            EndTime: `${new Date(timesheet.EndTime*1000).getHours() < 10 ? `0${new Date(timesheet.EndTime*1000).getHours()}` : new Date(timesheet.EndTime*1000).getHours()}:${new Date(timesheet.EndTime*1000).getMinutes() < 10 ? `0${new Date(timesheet.EndTime*1000).getMinutes()}` : new Date(timesheet.EndTime*1000).getMinutes()}`,
             MealBreak: String(timesheet.Mealbreak).split('T')[1].split('+')[0].split(':')[0].concat(':', String(timesheet.Mealbreak).split('T')[1].split('+')[0].split(':')[1]),
-            TotalTime: timesheet.TotalTime
+            TotalTime: timesheet.TotalTime,
+            TimeApproved: timesheet.TimeApproved,
+            Discarded: timesheet.Discarded
         })
     })
     return auxArray
